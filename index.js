1d745af4e8c68361f6f3cb368815f0792d01b0c9
Antor-Chowdhury <antor1347@gmail.com>
2025-12-17 01:31:12 +0600

    created collection for user & fetching that role usingpost

--- index.js
+++ index.js
@@ -8,6 +8,60 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://Blood-Donation:XM5aTmZsJIIrW2Wm@cluster0.enqqoua.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const database = client.db("Blood-Donation");
+    const userCollections = database.collection("user");
+
+    // collection for common user
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.role = "donor";
+      userInfo.status = "active";
+      userInfo.createdAt = new Date();
+
+      const result = await userCollections.insertOne(userInfo);
+
+      res.send(result);
+    });
+
+    // fetching role of the user
+    app.get("/users/role/:email", async (req, res) => {
+      const { email } = req.params;
+
+      const query = { email: email };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("From Assignment 11");
 });
